d0bad8a2900e2633dfea83ef0a9fac22c2b1de0f
brandon-hastings <65084955+brandon-hastings@users.noreply.github.com>
2023-06-27 17:02:20 -0400

    Add segment gui functions to segment.py

--- segment.py
+++ segment.py
@@ -1,13 +1,21 @@
+import os
 from skimage import io
 from sklearn.cluster import MiniBatchKMeans
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from pathlib import Path
 import numpy as np
+import fnmatch
 import sys
 import cv2
+import utils
+import tkinter as tk
 
 
-def main(file=None, N_cluster=None):
+'''MAIN SEGMENTATION METHOD VIA KMEANS SKIMAGE, CALLABLE FROM COMMAND LINE OR GUI'''
+
+
+def sk_segment(file=None, N_cluster=None):
     trigger = False
     if None not in (file, N_cluster):
         file = file
@@ -69,6 +77,9 @@ def main(file=None, N_cluster=None):
         return [fig, p, m, n]
 
 
+'''FUNCTIONS BELOW ARE FOR GUI FUNCTIONALITY'''
+
+
 def cv_segment(image, N_cluster):
 
     # image = image[:, :, :3]
@@ -81,7 +92,8 @@ def cv_segment(image, N_cluster):
     for i in range(2, N_cluster+1):
         pixel_vals = np.float32(pixel_vals)
 
-        # using cv2 kmeans clustering here as it gives better visual representation of image clustering to user than sklearn
+        '''using cv2 kmeans clustering here as it gives better visual representation of
+        image clustering to user than sklearn'''
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
         compactness, labels, centers = cv2.kmeans(pixel_vals, i, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
         # wcss.append(compactness)
@@ -98,6 +110,66 @@ def cv_segment(image, N_cluster):
         axs[i, 1].axis('off')
         axs[i, 1].text(50, 200, str(i+2), c='r', fontsize=10)
     return fig
-    
+
+
+def segment_gui(folder, N_cluster, toplevel, uv=False):
+    folder = utils.correctPath(folder)
+    if os.path.exists(Path(folder) / ".DS_Store"):
+        os.remove(Path(folder) / ".DS_Store")
+
+    # for every file in folder run a function that returns the modified image after input to a different function to
+    # save
+    def choose_clusters(file_path):
+        subroot = tk.Toplevel(toplevel)
+        subroot.title(str(file)+" ORIGINAL")
+
+        image = io.imread(file_path).astype(np.uint32)
+        if uv:
+            image[image == np.nan] = 0
+
+        image = image[:, :, :3]
+
+        def choose_segments(n_cluster):
+            seg_root = tk.Toplevel(subroot)
+            seg_root.title(str(file)+" CLUSTERED")
+            # image = io.imread(Path(folder) / file).astype(np.uint32)
+            # n_cluster = int(n_cluster) + 2
+            n_cluster = int(n_cluster)
+            fig, p, m, n = sk_segment(file_path, n_cluster)
+
+            def save_mask(selection):
+                i = int(selection)
+                b = np.reshape((p == i) * 1, (m, n))
+                savefile = file[:-4] + '_' + str(n_cluster) + '_' + str(selection) + '.png'
+                io.imsave(Path(folder) / savefile, (image * np.repeat(b[:, :, np.newaxis], 3, axis=2)).astype(np.uint8))
+                seg_root.quit()
+
+            utils.image_selection(seg_root, n_cluster, save_mask, utils.pop_up(fig, seg_root), end=0)
+
+            def seg_closing():
+                seg_root.destroy()
+
+            seg_root.protocol("WM_WINDOW_DELETE", seg_closing)
+            tk.mainloop()
+            seg_root.destroy()
+            subroot.quit()
+
+        utils.image_selection(subroot, N_cluster, choose_segments, utils.pop_up(cv_segment(image=image[:, :, :3],
+                                                                                           N_cluster=N_cluster),
+                                                                                subroot), start=2, end=1)
+
+        def sub_closing():
+            subroot.destroy()
+            sys.exit()
+
+        subroot.protocol("WM_WINDOW_DELETE", sub_closing)
+        tk.mainloop()
+        subroot.destroy()
+
+    for file in fnmatch.filter(sorted(os.listdir(folder)), '*e?.png'):
+        file_path = Path(folder) / file
+        choose_clusters(file_path)
+
+
 if __name__ == '__main__':
-    main()
+    sk_segment()
